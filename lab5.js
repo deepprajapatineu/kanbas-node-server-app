e0d7c3c4181700cb7e9d3e193e492760cb16e78f
Deep Prajapati <prajapati.de@northeastern.edu>
2023-11-20 20:33:19 -0500

    remove unnecessary lines

--- lab5.js
+++ lab5.js
@@ -83,9 +83,6 @@ const Lab5 = (app) => {
 	})
 
 	// Create a new TODO
-
-	// Not the right waof creating as we have POST
-
 	app.get("/a5/todos/create", (req, res) => {
 		const newTodo = {
 			id: new Date().getTime(),
@@ -112,18 +109,9 @@ const Lab5 = (app) => {
 			res.json(todo)
 		} else {
 			// We need to populate something
-			res.json("Sorry, there is not any TO with this ID,lease try again with valid ID!!")
+			res.json("Sorry, there is not any TODO with this ID,please try again with valid ID!!")
 		}
 	})
-
-	// Deleting a TODO
-
-	// app.get("/a5/todos/:id/delete", (req, res) => {
-	// 	const { id } = req.params
-	// 	const todo = todos.find((t) => t.id === parseInt(id))
-	// 	todos.splice(todos.indexOf(todo), 1)
-	// 	res.json(todos)
-	// })
 
 	app.delete("/a5/todos/:id", (req, res) => {
 		const { id } = req.params
@@ -183,81 +171,3 @@ const Lab5 = (app) => {
 	})
 }
 export default Lab5
-
-// const assignment = {
-//     id: 1,
-//     title: "NodeJS Assignment",
-//     description: "Create a NodeJS server with ExpressJS",
-//     due: "2021-10-10",
-//     completed: false,
-//     score: 0,
-//   };
-
-//   const todos = [
-//     { id: 1, title: "Task 1", completed: false },
-//     { id: 2, title: "Task 2", completed: false },
-//     { id: 3, title: "Task 3", completed: false },
-//     { id: 4, title: "Task 4", completed: false },
-//   ];
-  
-
-  
-// const Lab5 = (app) => {
-//     app.get("/a5/todos", (req, res) => {
-//         res.json(todos);
-//       });
-    
-//       app.get("/a5/todos/:id", (req, res) => {
-//         const { id } = req.params;
-//         const todo = todos.find((t) => t.id === parseInt(id));
-//         res.json(todo);
-//       });
-    
-    
-//     app.get("/a5/assignment", (req, res) => {
-//         res.json(assignment);
-//       });
-
-//       app.get("/a5/assignment/title", (req, res) => {
-//         res.json(assignment.title);
-//       });
-
-//       app.get("/a5/assignment/title/:newTitle", (req, res) => {
-//         const { newTitle } = req.params;
-//         assignment.title = newTitle;
-//         res.json(assignment);
-//       });
-    
-    
-    
-//     app.get("/a5/welcome", (req, res) => {
-//       res.send("Welcome to Assignment 5");
-//     });
-//     app.get("/a5/add/:a/:b", (req, res) => {
-//       const { a, b } = req.params;
-//       const sum = parseInt(a) + parseInt(b);
-//       res.send(sum.toString());
-//     });
-//     app.get("/a5/subtract/:a/:b", (req, res) => {
-//       const { a, b } = req.params;
-//       const sum = parseInt(a) - parseInt(b);
-//       res.send(sum.toString());
-//     });
-//     app.get("/a5/calculator", (req, res) => {
-//         const { a, b, operation } = req.query;
-//         let result = 0;
-//         switch (operation) {
-//           case "add":
-//             result = parseInt(a) + parseInt(b);
-//             break;
-//           case "subtract":
-//             result = parseInt(a) - parseInt(b);
-//             break;
-//           default:
-//             result = "Invalid operation";
-//         }
-//         res.send(result.toString());
-//       });
-    
-//   };
-//   export default Lab5;
